f4a33c50d6558a68aab70cf7b26a83c710fbdc79
Ammar Faizi <ammarfaizi2@gmail.com>
2019-07-26 21:11:18 +0700

    PHP c preprocessor

---
+++ src/ext/lang/en.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang = "en";
+$targetFile = __DIR__."/{$lang}.h";
+$langData = [
+	"start.group" => "This command can only be used in private.",
+	"start.private" => "Send /help to show the command list."
+];

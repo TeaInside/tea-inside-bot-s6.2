1b4f2c6a868a3faa71f35f352f029ed5af8ae0d4
Ammar Faizi <ammarfaizi2@gmail.com>
2019-09-14 15:02:34 +0700

    Prepare group logger

---
+++ src/classes/TeaBot/Loggers/GroupLogger.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace TeaBot\Loggers;
+
+use DB;
+use PDO;
+use TeaBot\LoggerFoundation;
+use TeaBot\Contracts\LoggerInterface;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \TeaBot\Loggers
+ * @version 6.2.0
+ */
+final class GroupLogger extends LoggerFoundation implements LoggerInterface
+{
+	/**
+	 * @return void
+	 */
+	public function run(): void
+	{
+		$this->saveUserInfo();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function saveUserInfo(): void
+	{
+		$createHistory = false;
+		$st = $this->pdo->prepare("SELECT `username`, `first_name`, `last_name`, `photo` FROM `users` WHERE `user_id` = :user_id LIMIT 1;");
+		$st->execute([":user_id" => $this->data["user_id"]]);
+		$data = [
+			":user_id" => $this->data["user_id"],
+			":username" => $this->data["username"],
+			":first_name" => $this->data["first_name"],
+			":last_name" => $this->data["last_name"],
+			":photo" => null,
+			":created_at" => date("Y-m-d H:i:s")
+		];
+
+		if ($r = $st->fetch(PDO::FETCH_ASSOC)) {
+			$this->pdo->prepare("UPDATE `users` SET `group_msg_count` = `group_msg_count` + 1 WHERE `user_id` = :user_id LIMIT 1;")->execute([":user_id" => $data[":user_id"]]);
+			if (
+				($this->data["username"] !== $r["username"]) ||
+				($this->data["first_name"] !== $r["first_name"]) ||
+				($this->data["last_name"] !== $r["last_name"])
+			) {
+				$createHistory = true;
+			}
+		} else {
+			$data[":is_bot"] = ($this->data["is_bot"] ? '1' : '0');
+			$this->pdo->prepare("INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `photo`, `is_bot`, `group_msg_count`, `private_msg_count`, `created_at`, `updated_at`) VALUES (:user_id, :username, :first_name, :last_name, :photo, :is_bot, 1, 0, :created_at, NULL);")->execute($data);
+			unset($data[":is_bot"]);
+			$data[":user_id"] = $this->pdo->lastInsertId();
+			$createHistory = true;
+		}
+
+		if ($createHistory) {
+			$this->pdo->prepare("INSERT INTO `users_history` (`user_id`, `username`, `first_name`, `last_name`, `photo`, `created_at`) VALUES (:user_id, :username, :first_name, :last_name, :photo, :created_at);")->execute($data);
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	public function logText(): void
+	{
+		$this->pdo
+			->prepare("INSERT INTO `group_messages` (`user_id`, `tmsg_id`, `reply_to_tmsg_id`, `msg_type`, `text`, `text_entities`, `file`, `is_edited`, `tmsg_datetime`, `created_at`) VALUES (:user_id, :tmsg_id, :reply_to_tmsg_id, :msg_type, :text, :text_entities, NULL, :is_edited, :tmsg_datetime, :created_at);")
+			->execute(
+				[
+					":user_id" => $this->data["user_id"],
+					":tmsg_id" => $this->data["msg_id"],
+					":reply_to_tmsg_id" => (
+						isset($this->data["reply"]) ? $this->data["reply"]["message_id"] : null
+					),
+					":msg_type" => "text",
+					":text" => $this->data["text"],
+					":text_entities" => (
+						isset($this->data["entities"]) ? json_encode($this->data["entities"], JSON_UNESCAPED_SLASHES) : null
+					),
+					":is_edited" => '0',
+					":tmsg_datetime" => date("Y-m-d H:i:s", $this->data["date"]),
+					":created_at" => date("Y-m-d H:i:s")
+				]
+			);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function logPhoto(): void
+	{
+	}
+}
--- src/classes/TeaBot/Loggers/PrivateLogger.php
+++ src/classes/TeaBot/Loggers/PrivateLogger.php
@@ -51,9 +51,8 @@ final class PrivateLogger extends LoggerFoundation implements LoggerInterface
 			}
 		} else {
 			$data[":is_bot"] = ($this->data["is_bot"] ? '1' : '0');
-			$data[":group_msg_count"] = 0;
-			$this->pdo->prepare("INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `photo`, `is_bot`, `group_msg_count`, `private_msg_count`, `created_at`, `updated_at`) VALUES (:user_id, :username, :first_name, :last_name, :photo, :is_bot, :group_msg_count, 1, :created_at, NULL);")->execute($data);
-			unset($data[":is_bot"], $data[":group_msg_count"]);
+			$this->pdo->prepare("INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `photo`, `is_bot`, `group_msg_count`, `private_msg_count`, `created_at`, `updated_at`) VALUES (:user_id, :username, :first_name, :last_name, :photo, :is_bot, 0, 1, :created_at, NULL);")->execute($data);
+			unset($data[":is_bot"]);
 			$data[":user_id"] = $this->pdo->lastInsertId();
 			$createHistory = true;
 		}
@@ -94,6 +93,5 @@ final class PrivateLogger extends LoggerFoundation implements LoggerInterface
 	 */
 	public function logPhoto(): void
 	{
-		
 	}
 }

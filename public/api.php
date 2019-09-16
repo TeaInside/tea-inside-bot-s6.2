26169006423491ec2ca0953c24a5ae3d93912125
Ammar Faizi <ammarfaizi2@gmail.com>
2019-09-16 10:34:38 +0700

    Clean up

---
+++ public/api.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__."/../bootstrap/autoload.php";
+
+use TeaBot\API\GetGroupMessages;
+
+if (isset($_GET["action"], $_GET["key"]) && is_string($_GET["action"]) && is_string($_GET["key"])) {
+
+	if ($_GET["key"] !== "e295c4659f3869f1c80129f28251ef11b30a4df1") {
+		goto forbidden;
+	}
+
+	switch ($_GET["action"]) {
+		case "get_group_messages":
+			$st = new GetGroupMessages();
+		break;
+			
+		default:
+		break;
+	}
+	$st->dispatch();
+	exit;
+}
+
+
+forbidden:
+header("Content-Type: application/json");
+http_response_code(403);
+print json_encode(
+	[
+		"error_code" => 403,
+		"error_message" => "Unauthorized"
+	]
+);
+exit;
---
+++ src/classes/TeaBot/API/GetGroupMessages.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace TeaBot\API;
+
+use DB;
+use PDO;
+
+class GetGroupMessages
+{
+	/**
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->pdo = DB::pdo();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function dispatch(): void
+	{
+		header("Content-Type: application/json");
+
+		if (isset($_GET["limit"]) && is_numeric($_GET["limit"]) && ($_GET["limit"] >= 0)) {
+			$limit = (int)$_GET["limit"];
+		} else {
+			$limit = 30;
+		}
+
+		if (isset($_GET["offset"]) && is_numeric($_GET["offset"]) && ($_GET["offset"] >= 0)) {
+			$offset = (int)$_GET["offset"];
+		} else {
+			$offset = 0;
+		}
+
+		print "{\"success\":true,\"param\":{\"limit\":{$limit},\"offset\":{$offset}},\"data\":[";
+
+		$st = $this->pdo->prepare("SELECT * FROM `group_messages` LIMIT {$limit} OFFSET {$offset};");
+		$st->execute();
+
+		$i = 0;
+		while ($r = $st->fetch(PDO::FETCH_ASSOC)) {
+			print ($i ? "," : "").json_encode($r, JSON_UNESCAPED_SLASHES);
+			$i++;
+		}
+
+		print "]}";
+	}
+}

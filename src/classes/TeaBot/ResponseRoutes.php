19746da7e1259a6c88c96788db93cd14695500fe
Ammar Faizi <ammarfaizi2@gmail.com>
2019-10-15 21:03:43 +0700

    Prepare calculus handler

--- src/classes/TeaBot/ResponseRoutes.php
+++ src/classes/TeaBot/ResponseRoutes.php
@@ -85,7 +85,7 @@ trait ResponseRoutes
 			$m[1] = (int)$m[1];
 			switch ($m[1]) {
 				case 1:
-					if ($this->stExec(TeaBot\Responses\Calculus::class, "simple", [$m[2]])) {
+					if ($this->stExec(Responses\Calculus::class, "simple", [$m[2]])) {
 						return true;
 					}
 				break;

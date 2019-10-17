0cdc371ac13f2fe147bd32132dbda0e0d01513ec
Ammar Faizi <ammarfaizi2@gmail.com>
2019-10-17 19:11:33 +0700

    Update d

--- src/classes/TeaBot/Responses/GoogleTranslate.php
+++ src/classes/TeaBot/Responses/GoogleTranslate.php
@@ -27,7 +27,7 @@ final class GoogleTranslate extends ResponseFoundation
 		try {
 			$text = str_replace("\n", ".%.", $text);
 			$st = new GoogleTranslateBase($source, $to, $text);
-			$res = str_replace(".%.", "\n", $st->execute());
+			$res = str_replace([".%.", ".% ."], "\n", $st->execute());
 			Exe::sendMessage(
 				[
 					"chat_id" => $this->data["chat_id"],

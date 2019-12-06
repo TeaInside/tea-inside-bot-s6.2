4c2e4b4f1e3e74f32200a66652021bdf75f7b247
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-06 19:47:29 +0700

    Clean up

--- src/classes/TeaBot/CaptchaHandler.php
+++ src/classes/TeaBot/CaptchaHandler.php
@@ -151,6 +151,7 @@ final class CaptchaHandler
 
     public static function checkAnswer(string $file, $answer = null, $extra = null): bool
     {
+        var_dump($answer, $extra);
         $checkAnswer = true;
         return require $file;
     }

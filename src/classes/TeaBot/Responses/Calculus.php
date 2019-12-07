ea76744ff05644524ab38a37230fce596b32c15e
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-07 22:48:47 +0700

    Update ax

--- src/classes/TeaBot/Responses/Calculus.php
+++ src/classes/TeaBot/Responses/Calculus.php
@@ -249,7 +249,7 @@ final class Calculus extends ResponseFoundation
 
         $e = explode("asynchronousPod('", $o, 2);
         if (count($e) < 2) goto invalid;
-        $e = explode("'", $e[0], 2);
+        $e = explode("'", $e[1], 2);
         if (count($e) < 2) goto invalid;
         $e = $e[0];
 
@@ -284,7 +284,7 @@ final class Calculus extends ResponseFoundation
 
         $e = explode("src=\"", $o, 2);
         if (count($e) < 2) goto invalid;
-        $e = explode("\"", $e[0], 2);
+        $e = explode("\"", $e[1], 2);
         if (count($e) < 2) goto invalid;
         $e = $e[0];
 

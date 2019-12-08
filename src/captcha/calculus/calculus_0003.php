ddd3c1d982e0faac309a2dad0d24d0412c14e16a
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-08 19:46:27 +0700

    Update

--- src/captcha/calculus/calculus_0003.php
+++ src/captcha/calculus/calculus_0003.php
@@ -18,16 +18,24 @@ if (isset($checkAnswer)) {
 }
 
 $timeout = 300; // 5 minutes.
-$extra = rand(0, 12);
+
+$extra = rand(0, 8);
+
 $up1 = rand(10, 300);
 $up2 = rand(10, 300);
-$mul = rand(2, 5);
-$lw1 = $up1 - $mul;
-$lw2 = $up2 - 1;
+
+$mul1 = rand(2, 10);
+$mul2 = rand(2, 10);
+
+$lw1 = $up1 - $mul1;
+$lw2 = $up2 - $mul2;
+
 $latex = "\int_{".$lw1."}^{".$up1."} \int_{".$lw2."}^{".$up2."} \int_{0}^{\infty} (x^{".$extra."} e^{-x}) dx dy dz";
-$extra = factorial($extra) * $mul;
+
+$extra = factorial($extra) * $mul1 * $mul2;
 
 $hash = md5("=".factorial($extra));
+
 is_dir("/tmp/telegram/calculus_lock/") or mkdir("/tmp/telegram/calculus_lock/");
 file_put_contents("/tmp/telegram/calculus_lock/".$hash, time());
 

2663316e6b65524adedccf61736d818c947367f0
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-10 05:06:44 +0700

    Clean up

--- src/captcha/calculus/calculus_0004.php
+++ src/captcha/calculus/calculus_0004.php
@@ -69,7 +69,7 @@ file_put_contents("/tmp/telegram/calculus_lock/".$hash, time());
 
 $msg = "<b>Please solve this captcha problem to make sure you are a human or you will be kicked in 5 minutes. Reply your answer to this message!</b>\n\n{$val} the following expression!";
 
-$photo = "https://api.teainside.org/latex_x.php?border=140&d=400&exp=".urlencode($latex);
+$photo = "https://api.teainside.org/latex_x.php?border=200&d=400&exp=".urlencode($latex);
 
 return [
     "timeout" => 300,

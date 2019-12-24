ebb9a25d67d75a2b2303d18108a9810ac8bf7d03
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-25 00:35:09 +0700

    Clean up

--- src/classes/TeaBot/ResponseRoutes.php
+++ src/classes/TeaBot/ResponseRoutes.php
@@ -109,6 +109,8 @@ trait ResponseRoutes
         /**
          * Google translate reply.
          */
+        var_dump(preg_match("/^(?:\/|\!|\~|\.)?(?:tl?r)\s(\S+)\s(\S+)$/Usi", $this->data["text"], $m),
+        $this->data["reply"]);
         if (preg_match("/^(?:\/|\!|\~|\.)?(?:tl?r)\s(\S+)\s(\S+)$/Usi", $this->data["text"], $m) &&
             (
                 (

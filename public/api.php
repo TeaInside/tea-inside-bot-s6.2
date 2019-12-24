b4c914f766934a40989f3751629058fde625d335
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-24 22:08:29 +0700

    Clean up api

--- public/api.php
+++ public/api.php
@@ -2,7 +2,10 @@
 
 header("Content-Type: application/json");
 
+
+
 if ($_GET["key"] === "fd2554a4ea62d1804805b89b2ea823a0ea17980fd56fadcbadac2f8d791b") {
+	require __DIR__."/../bootstrap/autoload.php";
 	switch ($_GET["action"]) {
 		case "asl_msg":
 			if (isset($_GET["msg_id"], $_GET["chat_id"]) &&
@@ -21,6 +24,13 @@ if ($_GET["key"] === "fd2554a4ea62d1804805b89b2ea823a0ea17980fd56fadcbadac2f8d79
 				} else {
 					@file_put_contents($f.".lock", time());
 				}
+				\TeaBot\Exe::sendMessage(
+					[
+						"chat_id" => $chatId,
+						"reply_to_message_id" => $msgId,
+						"text" => "Eligible ASL reply from @SrabatSrobot has been confirmed."
+					]
+				);
 				echo json_encode(["result" => "ok"]);
 				exit;
 			}
@@ -29,3 +39,6 @@ if ($_GET["key"] === "fd2554a4ea62d1804805b89b2ea823a0ea17980fd56fadcbadac2f8d79
 }
 http_response_code(400);
 echo json_encode(["result" => "invalid parameter"]);
+
+https://telegram-bot.teainside.org/api.php?key={key}&chat_id={xxx}&msg_id={yyy}&user_id={zzz}
+fd2554a4ea62d1804805b89b2ea823a0ea17980fd56fadcbadac2f8d791b

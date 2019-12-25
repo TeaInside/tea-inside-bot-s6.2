f1db88911f50d3d00ac0e7e919752eb3254aa181
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-25 07:49:56 +0700

    Clean up

--- src/classes/TeaBot/API/Chart/Chart.php
+++ src/classes/TeaBot/API/Chart/Chart.php
@@ -7,77 +7,34 @@ use PDO;
 
 class Chart
 {
+
+	/**
+	 * @param float $timeZone
+	 * @return array
+	 */
+	public static function rTimezone(float $timeZone): array
+	{
+		$r["sec"] = ((int)($timeZone * 3600));
+		$r["hz"] = sprintf(
+			"%s%02d:%02d",
+			$timeZone >= 0 ? "+" : "-",
+			abs($r["sec"]),
+			abs((($r["sec"] * 3600) % 3600) / 60)
+		);
+		return $r;
+	}
+
 	/**
 	 * @param string $startDate
 	 * @param string $endDate
+	 * @param float  $timeZone
 	 */
 	public static function messages(string $startDate, string $endDate, float $timeZone = 7)
 	{
-		// $pdo = DB::pdo();
-		// $st = $pdo->prepare("
-		// 	SELECT 1 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','+07:00')) as `date` FROM `groups_messages`
-		// 	WHERE `group_id` = -1001162202776 AND
-		// 	`tmsg_datetime` >= :start_date AND
-		// 	`tmsg_datetime` <= :end_date
-		// 	GROUP BY `date`
-
-		// 	UNION 
-
-		// 	SELECT 2 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','+07:00')) as `date` FROM `groups_messages`
-		// 	WHERE `group_id` = -1001120283944 AND
-		// 	`tmsg_datetime` >= :start_date AND
-		// 	`tmsg_datetime` <= :end_date
-		// 	GROUP BY `date`
-		// ");
-		// if (strlen($startDate) <= 7) {
-		// 	$startDate .= " 00:00:00";
-		// }
-		// if (strlen($endDate) <= 7) {
-		// 	$endDate .= " 23:59:59";
-		// }
-		// $st->execute(
-		// 	[
-		// 		":start_date" => date("Y-m-d H:i:s", strtotime($startDate) - ($timeZone * 3600)),
-		// 		":end_date" => date("Y-m-d H:i:s", strtotime($endDate) - ($timeZone * 3600))
-		// 	]
-		// );
-		// $res = [
-		// 	"labels" => [],
-		// 	"datasets" => [
-		// 		[
-		// 			"label" => "Koding Teh",
-		// 			"data" => [],
-		// 			"backgroundColor" => "red",
-		// 			"borderColor" => "red",
-		// 			"borderWidth" => 3,
-		// 			"fill" => false
-		// 		],
-		// 		[
-		// 			"label" => "Tea Inside Indonesia",
-		// 			"data" => [],
-		// 			"backgroundColor" => "green",
-		// 			"borderColor" => "green",
-		// 			"borderWidth" => 3,
-		// 			"fill" => false
-		// 		]
-		// 	]
-		// ];
-		// $r = $st->fetchAll(PDO::FETCH_ASSOC);
-		// foreach ($r as $k => $v) {
-		// 	if ($v["k"] == 1) {
-		// 		$res["labels"][] = date("d M Y", strtotime($v["date"]));
-		// 		$res["datasets"][0]["data"][] = $v["messages"];
-		// 	} else {
-		// 		$res["datasets"][1]["data"][] = $v["messages"];
-		// 	}
-		// }
-		// echo json_encode($res);
-		// DB::close();
-
-
 		$pdo = DB::pdo();
+		$tz = self::rTimezone($timeZone);
 		$st = $pdo->prepare("
-			SELECT 1 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','+07:00')) as `date` FROM `groups_messages`
+			SELECT 1 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','{$tz["hz"]}')) as `date` FROM `groups_messages`
 			WHERE `group_id` = -1001162202776 AND
 			`tmsg_datetime` >= :start_date AND
 			`tmsg_datetime` <= :end_date
@@ -85,7 +42,7 @@ class Chart
 
 			UNION 
 
-			SELECT 2 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','+07:00')) as `date` FROM `groups_messages`
+			SELECT 2 as `k`, COUNT(1) as `messages`, DATE(CONVERT_TZ(`tmsg_datetime`, '+00:00','{$tz["hz"]}')) as `date` FROM `groups_messages`
 			WHERE `group_id` = -1001120283944 AND
 			`tmsg_datetime` >= :start_date AND
 			`tmsg_datetime` <= :end_date
@@ -99,8 +56,8 @@ class Chart
 		}
 		$st->execute(
 			[
-				":start_date" => date("Y-m-d H:i:s", strtotime($startDate) - ($timeZone * 3600)),
-				":end_date" => date("Y-m-d H:i:s", strtotime($endDate) - ($timeZone * 3600))
+				":start_date" => date("Y-m-d H:i:s", strtotime($startDate) - $tz["sec"]),
+				":end_date" => date("Y-m-d H:i:s", strtotime($endDate) - $tz["sec"])
 			]
 		);
 		$res = [
@@ -127,7 +84,7 @@ class Chart
 		$r = $st->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($r as $k => $v) {
 			if ($v["k"] == 1) {
-				$res["labels"][] = date("d M Y H:i:s", strtotime($v["date"]));
+				$res["labels"][] = date("d M Y", strtotime($v["date"]));
 				$res["datasets"][0]["data"][] = $v["messages"];
 			} else {
 				$res["datasets"][1]["data"][] = $v["messages"];
@@ -140,10 +97,12 @@ class Chart
 	/**
 	 * @param string $startDate
 	 * @param string $endDate
+	 * @param float  $timeZone
 	 */
-	public static function userStats(string $startDate, string $endDate)
+	public static function userStats(string $startDate, string $endDate, float $timeZone = 7)
 	{
 		$pdo = DB::pdo();
+		$tz = self::rTimezone($timeZone);
 		$st = $pdo->prepare("
 			SELECT * FROM (SELECT
 				1 as `k`,
@@ -195,8 +154,8 @@ class Chart
 		");
 		$st->execute(
 			[
-				":start_date" => date("Y-m-d 00:00:00", strtotime($startDate)),
-				":end_date" => date("Y-m-d 23:59:59", strtotime($endDate))
+				":start_date" => date("Y-m-d 00:00:00", strtotime($startDate) - $tz["sec"]),
+				":end_date" => date("Y-m-d 23:59:59", strtotime($endDate) - $tz["sec"])
 			]
 		);
 		echo json_encode($st->fetchAll(PDO::FETCH_NUM));

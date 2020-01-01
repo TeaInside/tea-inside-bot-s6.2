7bfe95623718f351cee6583b44b641e12f9d2be7
Ammar Faizi <ammarfaizi2@gmail.com>
2020-01-01 22:54:14 +0700

    Update chat_id

--- bin/paxel_alarm.php
+++ bin/paxel_alarm.php
@@ -19,50 +19,50 @@ $groupId = -1001491638996;
 $u = json_decode(file_get_contents(PAXEL_TG."/976357499"), true);
 
 while (true) {
-	$px = new BasePaxel($u["username"], $u["password"]);
+    $px = new BasePaxel($u["username"], $u["password"]);
 
-	$newPackage = $px->package(true);
-	if (file_exists(PAXEL_DIR."/package.json")) {
-		$oldPackage = file_get_contents(PAXEL_DIR."/package.json");
-	} else {
-		$oldPackage = "";
-	}
+    $newPackage = $px->package(true);
+    if (file_exists(PAXEL_DIR."/package.json")) {
+        $oldPackage = file_get_contents(PAXEL_DIR."/package.json");
+    } else {
+        $oldPackage = "";
+    }
 
-	if ($newPackage && ($newPackage !== $oldPackage)) {
-		$r = "[".date("d F Y H:i:s")."]\n\nSome changes on package list were made!\n\n";
-		$package = json_decode($newPackage, true);
-		$oPackage = json_decode($oldPackage, true);
+    if ($newPackage && ($newPackage !== $oldPackage)) {
+        $r = "[".date("d F Y H:i:s")."]\n\nSome changes on package list were made!\n\n";
+        $package = json_decode($newPackage, true);
+        $oPackage = json_decode($oldPackage, true);
 
-		foreach ($package["data"] as $k => $v) {
-			foreach ($oPackage["data"] as $kk => $vv) {
-				if (sha1(json_encode($v)) === sha1(json_encode($vv))) {
-					unset($package["data"][$k]);
-				}
-			}
-		}
+        foreach ($package["data"] as $k => $v) {
+            foreach ($oPackage["data"] as $kk => $vv) {
+                if (sha1(json_encode($v)) === sha1(json_encode($vv))) {
+                    unset($package["data"][$k]);
+                }
+            }
+        }
 
-		$package["data"] = array_values($package["data"]);
+        $package["data"] = array_values($package["data"]);
 
-		foreach ($package["data"] as $k => $v) {
-	        foreach ($v as $kk => $vv) {
-	            $r .= "<b>".htmlspecialchars(ucfirst($kk), ENT_QUOTES).
-	                ":</b> ".htmlspecialchars($vv, ENT_QUOTES)."\n";
-	        }
-	        $r .= "\n";
-	    }
+        foreach ($package["data"] as $k => $v) {
+            foreach ($v as $kk => $vv) {
+                $r .= "<b>".htmlspecialchars(ucfirst($kk), ENT_QUOTES).
+                    ":</b> ".htmlspecialchars($vv, ENT_QUOTES)."\n";
+            }
+            $r .= "\n";
+        }
 
-		file_put_contents(PAXEL_DIR."/package.json", $newPackage);
-		TeaBot\Exe::sendMessage(
-			[
-				"chat_id" => $groupId,
-				"text" => $r,
-				"parse_mode" => "HTML"
-			]
-		);
-		echo "There are some changes!\n";
-	} else {
-		echo "No changes!\n";
-	}
+        file_put_contents(PAXEL_DIR."/package.json", $newPackage);
+        TeaBot\Exe::sendMessage(
+            [
+                "chat_id" => $groupId,
+                "text" => $r,
+                "parse_mode" => "HTML"
+            ]
+        );
+        echo "There are some changes!\n";
+    } else {
+        echo "No changes!\n";
+    }
 
-	sleep(15);
+    sleep(15);
 }

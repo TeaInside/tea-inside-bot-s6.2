484454517fe034705ee6ab07123fbf23c3645a85
Ammar Faizi <ammarfaizi2@gmail.com>
2020-01-04 22:13:40 +0700

    Update paxel alarm

--- bin/paxel_extra.php
+++ bin/paxel_extra.php
@@ -2,29 +2,40 @@
 
 const PAXEL_EXTRA = [
 
-	"cilokcepot" => [
-		"lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
-		"add_words" => "Cilok Cepot"
-	],
+    "cilokcepot" => [
+        "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
+        "add_words" => "Cilok Cepot"
+    ],
 
-	"baksomalang" => [
-		"lokasi_pickup" => "Paxel Home Malang",
-		"add_words" => "Bakso President"
-	],
+    "baksomalang" => [
+        "lokasi_pickup" => "Paxel Home Malang",
+        "add_words" => "Bakso President"
+    ],
 
-	"Yoyicfermented01" => [
-		"lokasi_pickup" => "Paxel Home terdekat",
-		"add_words" => "YoyiC Fermented Milk"
-	],
+    "Yoyicfermented01" => [
+        "lokasi_pickup" => "Paxel Home terdekat",
+        "add_words" => "YoyiC Fermented Milk"
+    ],
 
-	"Bunanik05" => [
-		"lokasi_pickup" => "Paxel Home Panjang",
-		"add_words" => "Bu Nanik"
-	],
+    "Bunanik05" => [
+        "lokasi_pickup" => "Paxel Home Panjang",
+        "add_words" => "Bu Nanik"
+    ],
 
-	"Rotinogat" => [
-		"lokasi_pickup" => "Paxel Home BSD",
-		"add_words" => "Roti Nogat"
-	]
+    "Rotinogat" => [
+        "lokasi_pickup" => "Paxel Home BSD",
+        "add_words" => "Roti Nogat"
+    ],
 
+
+    "Primarasabrownies" => [
+        "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
+        "add_words" => "Brownis Prima Rasa"
+    ],
+
+
+    "BatagorKingsley" => [
+        "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
+        "add_words" => "Batagor Kingsley"
+    ]
 ];

2c091bc658037e434a56a473d4d6467653046d77
Ammar Faizi <ammarfaizi2@gmail.com>
2020-01-04 22:39:07 +0700

    Update paxel_extra.php

--- bin/paxel_extra.php
+++ bin/paxel_extra.php
@@ -4,38 +4,47 @@ const PAXEL_EXTRA = [
 
     "cilokcepot" => [
         "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
-        "add_words" => "Cilok Cepot"
+        "add_words" => "Cilok Cepot",
+        "waktu_pickup_sabtu" => "Rabu, 16.00 - 18.00 WIB",
+        "waktu_pickup_rabu" => "Sabtu, 16.00 - 18.00 WIB"
     ],
 
     "baksomalang" => [
         "lokasi_pickup" => "Paxel Home Malang",
-        "add_words" => "Bakso President"
+        "add_words" => "Bakso President",
+        "waktu_pickup" => "Rabu, 14.00 - 16.00 WIB"
     ],
 
     "Yoyicfermented01" => [
         "lokasi_pickup" => "Paxel Home terdekat",
-        "add_words" => "YoyiC Fermented Milk"
+        "add_words" => "YoyiC Fermented Milk",
+        "estimasi_tiba" => "Esok hari, 12.00 - 14.00 WIB"
     ],
 
     "Bunanik05" => [
         "lokasi_pickup" => "Paxel Home Panjang",
-        "add_words" => "Bu Nanik"
+        "add_words" => "Bu Nanik",
+        "waktu_pickup_selasa" => "Sabtu, 10.00 - 12.00 WIB",
+        "waktu_pickup_kamis" => "Minggu, 10.00 - 12.00 WIB"
     ],
 
     "Rotinogat" => [
         "lokasi_pickup" => "Paxel Home BSD",
-        "add_words" => "Roti Nogat"
+        "add_words" => "Roti Nogat",
+        "waktu_pickup" => "Sabtu, 10.00 - 12.00 WIB"
     ],
 
 
     "Primarasabrownies" => [
         "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
-        "add_words" => "Brownis Prima Rasa"
+        "add_words" => "Brownis Prima Rasa",
+        "waktu_pickup" => "Senin, 20.00 - 22.00 WIB"
     ],
 
 
     "BatagorKingsley" => [
         "lokasi_pickup" => "Paxel Home Bandung Lemahnendeut",
-        "add_words" => "Batagor Kingsley"
+        "add_words" => "Batagor Kingsley",
+        "waktu_pickup" => "Senin, 20.00 - 22.00 WIB"
     ]
 ];

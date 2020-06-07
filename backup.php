8dfd66b2879a203d50934401f857f0e5297d6ff6
Ammar Faizi <ammarfaizi2@gmail.com>
2020-06-07 17:33:19 +0700

    Update backup

--- backup.php
+++ backup.php
@@ -22,7 +22,7 @@ $jobs["teabot"] = function () {
     " --default-character-set=utf8mb4 --hex-blob teabot | gzip -9 > ".
     escapeshellarg($filename)." 2>&1"
   );
-  sendFile($filename);
+  return sendFile($filename);
 };
 
 
@@ -38,7 +38,7 @@ $jobs["rsudciamis"] = function () {
     " --default-character-set=utf8mb4 --hex-blob rsudciamis_antrian | gzip -9 > ".
     escapeshellarg($filename)." 2>&1"
   );
-  sendFile($filename);
+  return sendFile($filename);
 };
 
 
@@ -46,7 +46,7 @@ $jobs["srabatsrobot_v3"] = function () {
   $now = date("Y_m_d__H_i_s");
   $filename = "/tmp/srabatsrobot_v3_".$now.".tar.gz";
   shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename)." 2>&1");
-  sendFile($filename);
+  return sendFile($filename);
 };
 
 
@@ -54,19 +54,23 @@ $jobs["thefox"] = function () {
   $now = date("Y_m_d__H_i_s");
   $filename = "/tmp/thefox_".$now.".tar.gz";
   shell_exec("cd /home/; tar -c thefox | gzip -9 > ".escapeshellarg($filename));
-  sendFile($filename);
+  return sendFile($filename);
 };
 
 
 $aStart = microtime(true);
 $r = "Time taken for each job:\n";
+$r2 = "File size for each job:\n";
+$totalSize = 0;
 foreach ($jobs as $k => $callback) {
   $start = microtime(true);
-  $callback();
+  $filesize = $callback();
   $k = htmlspecialchars($k, ENT_QUOTES, "UTF-8");
   $r .= "<b>{$k}</b> = <code>".round(microtime(true) - $start, 6)."</code> s\n";
+  $r .= "<b>{$k}</b> = <code>".$filesize."</code> bytes\n";
+  $totalSize += $filesize;
 }
-$r .= "\nTotal time taken: <code>".round(microtime(true) - $aStart, 6)."</code>";
+$r .= "\nTotal time taken: <code>".round(microtime(true) - $aStart, 6)."</code> s";
 
 var_dump(\TeaBot\Exe::sendMessage(
   [
@@ -79,6 +83,8 @@ var_dump(\TeaBot\Exe::sendMessage(
 
 function sendFile($filename)
 {
+  $filesize = filesize($filename);
+
   $caption =
     "filename: ".basename($filename).
     "\ncreated_at: ".date("Y-m-d H:i:s").
@@ -101,4 +107,6 @@ function sendFile($filename)
   echo curl_exec($ch);
   curl_close($ch);
   unlink($filename);
+
+  return $filesize;
 }

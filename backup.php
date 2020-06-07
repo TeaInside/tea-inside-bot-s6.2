37b50a97c510191cf2bdf523806f68f020e06440
Ammar Faizi <ammarfaizi2@gmail.com>
2020-06-07 10:41:16 +0700

    Update backup.php

--- backup.php
+++ backup.php
@@ -5,12 +5,20 @@ require __DIR__."/bootstrap/autoload.php";
 loadConfig("telegram_bot");
 
 $now = date("Y_m_d__H_i_s");
-$caption = "srabatsrobot backup ".date("Y-m-d H:i:s");
+$filename = "/tmp/srabatsrobot_backup_v3_".$now.".tar.gz";
 
-shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz /tmp/v3.tar.gz;");
+echo "Compressing data...\n";
+shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename));
 
 
-if (file_exists("/tmp/v3.tar.gz")) {
+if (file_exists($filename)) {
+
+  $caption = basename($filename).
+    "\n".date("Y-m-d H:i:s").
+    "\nmd5:".md5_file($filename).
+    "\nsha1:".sha1_file($filename);
+
+
   $ch = curl_init("https://api.telegram.org/bot".BOT_TOKEN."/sendDocument");
   $optf = [
     CURLOPT_RETURNTRANSFER => true,
@@ -20,11 +28,11 @@ if (file_exists("/tmp/v3.tar.gz")) {
     CURLOPT_POSTFIELDS => [
       "caption" => $caption,
       "chat_id" => -1001226735471,
-      "document" => new \CurlFile("/tmp/v3.tar.gz")
+      "document" => new \CurlFile($filename)
     ]
   ];
   curl_setopt_array($ch, $optf);
   echo curl_exec($ch);
   curl_close($ch);
-  unlink("/tmp/v3.tar.gz");
+  unlink($filename);
 }

b7203c3195982624d98e5ee27f0666cb0e647de1
Ammar Faizi <ammarfaizi2@gmail.com>
2020-06-07 17:15:25 +0700

    Update backup.php

--- backup.php
+++ backup.php
@@ -6,40 +6,40 @@ require __DIR__."/config.php";
 loadConfig("telegram_bot");
 
 
-// $now = date("Y_m_d__H_i_s");
-// $filename = "/tmp/teabot_".$now.".sql.gz";
-// echo shell_exec(
-//   "mysqldump ".
-//   " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
-//   " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
-//   " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
-//   " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
-//   " --default-character-set=utf8mb4 --hex-blob teabot | gzip -9 > ".
-//   escapeshellarg($filename)." 2>&1"
-// );
-// sendFile($filename);
+$now = date("Y_m_d__H_i_s");
+$filename = "/tmp/teabot_".$now.".sql.gz";
+echo shell_exec(
+  "mysqldump ".
+  " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
+  " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
+  " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
+  " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
+  " --default-character-set=utf8mb4 --hex-blob teabot | gzip -9 > ".
+  escapeshellarg($filename)." 2>&1"
+);
+sendFile($filename);
 
 
 
 
-// $now = date("Y_m_d__H_i_s");
-// $filename = "/tmp/rsudciamis_antrian_".$now.".sql.gz";
-// echo shell_exec(
-//   "mysqldump ".
-//   " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
-//   " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
-//   " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
-//   " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
-//   " --default-character-set=utf8mb4 --hex-blob rsudciamis_antrian | gzip -9 > ".
-//   escapeshellarg($filename)." 2>&1"
-// );
-// sendFile($filename);
+$now = date("Y_m_d__H_i_s");
+$filename = "/tmp/rsudciamis_antrian_".$now.".sql.gz";
+echo shell_exec(
+  "mysqldump ".
+  " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
+  " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
+  " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
+  " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
+  " --default-character-set=utf8mb4 --hex-blob rsudciamis_antrian | gzip -9 > ".
+  escapeshellarg($filename)." 2>&1"
+);
+sendFile($filename);
 
 
-// $now = date("Y_m_d__H_i_s");
-// $filename = "/tmp/srabatsrobot_v3_".$now.".tar.gz";
-// shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename)." 2>&1");
-// sendFile($filename);
+$now = date("Y_m_d__H_i_s");
+$filename = "/tmp/srabatsrobot_v3_".$now.".tar.gz";
+shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename)." 2>&1");
+sendFile($filename);
 
 
 $now = date("Y_m_d__H_i_s");
@@ -52,8 +52,8 @@ function sendFile($filename)
   $caption =
     "filename: ".basename($filename).
     "\ncreated_at: ".date("Y-m-d H:i:s").
-    "\nmd5:".md5_file($filename).
-    "\nsha1:".sha1_file($filename);
+    "\nmd5: ".md5_file($filename).
+    "\nsha1: ".sha1_file($filename);
 
   $ch = curl_init("https://api.telegram.org/bot".BOT_TOKEN."/sendDocument");
   $optf = [

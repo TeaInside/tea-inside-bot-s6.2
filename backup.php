492fe8f4058424276d9838e0cfb689a8b1e8426f
Ammar Faizi <ammarfaizi2@gmail.com>
2020-06-07 17:26:27 +0700

    Update backup

--- backup.php
+++ backup.php
@@ -8,46 +8,72 @@ require __DIR__."/config.php";
 loadConfig("telegram_bot");
 
 
-$now = date("Y_m_d__H_i_s");
-$filename = "/tmp/teabot_".$now.".sql.gz";
-echo shell_exec(
-  "mysqldump ".
-  " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
-  " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
-  " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
-  " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
-  " --default-character-set=utf8mb4 --hex-blob teabot | gzip -9 > ".
-  escapeshellarg($filename)." 2>&1"
+$jobs = [];
+
+$jobs["teabot"] = function () {
+  $now = date("Y_m_d__H_i_s");
+  $filename = "/tmp/teabot_".$now.".sql.gz";
+  echo shell_exec(
+    "mysqldump ".
+    " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
+    " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
+    " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
+    " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
+    " --default-character-set=utf8mb4 --hex-blob teabot | gzip -9 > ".
+    escapeshellarg($filename)." 2>&1"
+  );
+  sendFile($filename);
+};
+
+
+$jobs["rsudciamis"] = function () {
+  $now = date("Y_m_d__H_i_s");
+  $filename = "/tmp/rsudciamis_antrian_".$now.".sql.gz";
+  echo shell_exec(
+    "mysqldump ".
+    " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
+    " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
+    " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
+    " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
+    " --default-character-set=utf8mb4 --hex-blob rsudciamis_antrian | gzip -9 > ".
+    escapeshellarg($filename)." 2>&1"
+  );
+  sendFile($filename);
+};
+
+
+$jobs["srabatsrobot_v3"] = function () {
+  $now = date("Y_m_d__H_i_s");
+  $filename = "/tmp/srabatsrobot_v3_".$now.".tar.gz";
+  shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename)." 2>&1");
+  sendFile($filename);
+};
+
+
+$jobs["thefox"] = function () {
+  $now = date("Y_m_d__H_i_s");
+  $filename = "/tmp/thefox_".$now.".tar.gz";
+  shell_exec("cd /home/; tar -c thefox | gzip -9 > ".escapeshellarg($filename));
+  sendFile($filename);
+};
+
+
+$r = "Time taken for each job:\n";
+foreach ($jobs as $k => $callback) {
+  $start = microtime(true);
+  $callback();
+  $k = htmlspecialchars($k, ENT_QUOTES, "UTF-8");
+  $r .= "<b>{$k}</b> = <code>".round(microtime(true) - $start, 6)."</code> s\n";
+}
+
+Exe::sendMessage(
+  [
+    "chat_id" => -1001261147301,
+    "text" => $r,
+    "parse_mode" => "HTML"
+  ]
 );
-sendFile($filename);
 
-
-
-
-$now = date("Y_m_d__H_i_s");
-$filename = "/tmp/rsudciamis_antrian_".$now.".sql.gz";
-echo shell_exec(
-  "mysqldump ".
-  " -h ".escapeshellarg(TELEGRAM_BOT_DB_HOST).
-  " -P ".escapeshellarg(TELEGRAM_BOT_DB_PORT).
-  " -u ".escapeshellarg(TELEGRAM_BOT_DB_USER).
-  " -p".escapeshellarg(TELEGRAM_BOT_DB_PASS).
-  " --default-character-set=utf8mb4 --hex-blob rsudciamis_antrian | gzip -9 > ".
-  escapeshellarg($filename)." 2>&1"
-);
-sendFile($filename);
-
-
-$now = date("Y_m_d__H_i_s");
-$filename = "/tmp/srabatsrobot_v3_".$now.".tar.gz";
-shell_exec("cd /home/candragati; rm -vf v3.tar.gz; tar -c v3 | gzip -9 > v3.tar.gz; chown -R candragati:candragati v3.tar.gz; cp -vf v3.tar.gz ".escapeshellarg($filename)." 2>&1");
-sendFile($filename);
-
-
-$now = date("Y_m_d__H_i_s");
-$filename = "/tmp/thefox_".$now.".tar.gz";
-shell_exec("cd /home/; tar -c thefox | gzip -9 > ".escapeshellarg($filename));
-sendFile($filename);
 
 function sendFile($filename)
 {
